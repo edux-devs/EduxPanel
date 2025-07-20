48d367d91b10b710c7e4473bd697f22bbe708224
Edux-Devs <user@user>
2025-07-20 15:27:52 -0600

    add directory

---
+++ functions/banner.py
@@ -0,0 +1,10 @@
+def show_banner():
+    print("\033[34m ╔══════════════════════════════════════╗")
+    print("\033[34m ║\033[33m     >_ Edux Panel 1.0 (2025)         \033[0;34m║")
+    print("\033[34m ║\033[33m     ⌁ Sistema Inteligente de Dados   \033[34m║")
+    print("\033[34m ║\033[33m     ⌂ Terminal Ativado               \033[34m║")
+    print("\033[34m ╚══════════════════════════════════════╝\033[0m\n", end='')
+
+
+if __name__ == '__main__':
+    show_banner()
